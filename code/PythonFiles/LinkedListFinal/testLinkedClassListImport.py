d9b2ddd6ebffeeb4f6d1d898aa5a67b73b96e5c6
Aman Verma <amanvrm13@gmail.com>
2023-04-30 03:10:17 +0530

    del function added

--- code/PythonFiles/LinkedListFinal/testLinkedClassListImport.py
+++ code/PythonFiles/LinkedListFinal/testLinkedClassListImport.py
@@ -5,5 +5,8 @@ newLl = singlyLinkedList.LinkedList()
 newLl.insertAtBegining(0)
 newLl.insertAtBegining(1)
 newLl.insertAtBegining(3)
+newLl.delAtEnd()
 
-print( [ x.data for x in newLl] )
+ll = [ x.data for x in newLl]
+if ll:
+    print(ll)
